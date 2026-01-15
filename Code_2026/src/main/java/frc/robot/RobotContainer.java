d398bc4204f525087ffa29c20ae145dcaefbe9e4
Trollguyjr <icastaneda4474@tpaa.org>
2026-01-14 16:50:49 -0800

    swerve drive and yall

--- Code_2026/src/main/java/frc/robot/RobotContainer.java
+++ Code_2026/src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,15 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
@@ -19,12 +27,22 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+  private final SwerveSubsystem drivebase = new SwerveSubsystem();
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController driverXbox =
+      new CommandXboxController(OperatorConstants.DriverContrlPort);
+  
+  private final CommandXboxController operatorController = 
+      new CommandXboxController(OperatorConstants.OperatorContrlPort);
+
+  
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(), () -> driverXbox.getLeftY() * -1, () -> driverXbox.getLeftX() * -1)
+  .withControllerRotationAxis(() -> driverXbox.getRightX() * -1)
+  .deadband(OperatorConstants.DEADBAND)
+  .scaleTranslation(1.8)
+  .allianceRelativeControl(true);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -42,13 +60,17 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    Command drivefieldOrientedAngularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+    drivebase.setDefaultCommand(drivefieldOrientedAngularVelocity);
+    driverXbox.a().onTrue(
+      Commands.runOnce(() -> {
+        Pose2d currentPose = drivebase.getSwerveDrive().getPose();
+        Pose2d newPose = new Pose2d(
+          currentPose.getTranslation(), Rotation2d.fromDegrees(0)
+        );
+        drivebase.getSwerveDrive().resetOdometry(newPose);
+      })
+    );
   }
 
   /**
@@ -58,6 +80,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return new PathPlannerAuto("Cool");
   }
 }
